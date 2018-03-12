3c04952bf0968df491381de3daa20c45312646ea
Mohammed Akbar Ramzan <mohammed.ramzan@student.manchester.ac.uk>
2018-03-12 19:59:29 +0000

    Warning: plugins!

---
+++ meet-and-play/platforms/browser/www/build/9.js
@@ -0,0 +1,42 @@
+webpackJsonp([9],{
+
+/***/ 294:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AdditionalInfoPageModule", function() { return AdditionalInfoPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(10);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__additional_info__ = __webpack_require__(101);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var AdditionalInfoPageModule = (function () {
+    function AdditionalInfoPageModule() {
+    }
+    AdditionalInfoPageModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["I" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_2__additional_info__["a" /* AdditionalInfoPage */],
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__additional_info__["a" /* AdditionalInfoPage */]),
+            ],
+        })
+    ], AdditionalInfoPageModule);
+    return AdditionalInfoPageModule;
+}());
+
+//# sourceMappingURL=additional-info.module.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=9.js.map
